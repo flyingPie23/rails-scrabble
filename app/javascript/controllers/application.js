de116f2e90636b049ab72b27a73aeda40591b573
flyingPie23 <drissfreah325@gmail.com>
2024-02-15 17:28:38 +0100

    add

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,13 @@
 import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+
+application.load(definitionsFromContext(context))
 
 export { application }
--- config/initializers/openai.rb
+++
@@ -1,3 +0,0 @@
-OpenAI.configure do |config|
-  config.access_token = ENV.fetch("OPEN_AI_API_KEY")
-end
